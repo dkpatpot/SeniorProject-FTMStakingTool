b85a4567c9bc1a2e9628aaa3bcfedf348fda366a
kanisont <kanison.t@ku.th>
2024-02-16 19:43:20 +0700

    Update OptimalStaking.jsx

--- ftm-stakingtool/src/pages/OptimalStaking.jsx
+++ ftm-stakingtool/src/pages/OptimalStaking.jsx
@@ -7,28 +7,86 @@ function OptimalStaking() {
     return (
         <div>
             <Header/>
-            <Box sx = {{
-                width: 300,
-                height: 500,
-                borderRadius: 5,
-                mx: 'auto',
-                my: 10,
-                bgcolor: '#f0ad4e',
+            <Box
+                sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
-            }}>
-                <div>
-                    <TextField sx = {{
-                        my: 4,
+                justifyContent: 'center',
+                p: 1,
+                m: 1,
+                bgcolor: 'background.paper',
+                borderRadius: 1,
+                }}
+            >
+                <Box
+                    sx={{
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    flexDirection: 'column',
+                    p: 1,
+                    mx: 1,
+                    my: 0,
+                    bgcolor: 'background.paper',
+                    borderRadius: 1,
                     }}
-                        required
-                        id="returnsPercent"
-                        label="Returns Percent"
-                        type="search"
-                    />
-                </div>
+                >
+                    <Box sx = {{
+                        border: 2,
+                        borderBottom: 4,
+                        borderLeft: 4,
+                        p: 1,
+                        my: 0,
+                        mx: 0,
+                        width: 300,
+                        height: 230,
+                        borderRadius: 5,
+                        bgcolor: '#f0ad4e',
+                        }}>
+                    </Box>
+
+                    <Box sx = {{
+                        border: 2,
+                        borderBottom: 4,
+                        borderLeft: 4,
+                        p: 1,
+                        my: 4.8,
+                        mx: 0,
+                        width: 300,
+                        height: 230,
+                        borderRadius: 5,
+                        bgcolor: '#f0ad4e',
+                        }}>
+                    </Box>
+                </Box>
+
+                <Box sx = {{
+                        border: 2,
+                        borderBottom: 4,
+                        borderLeft: 4,
+                        p: 1,
+                        my: 1,
+                        mx: 2,
+                        width: 300,
+                        height: 500,
+                        borderRadius: 5,
+                        bgcolor: '#f0ad4e',
+                    }}>
+                </Box>
+
+                <Box sx = {{
+                        border: 2,
+                        borderBottom: 4,
+                        borderLeft: 4,
+                        p: 1,
+                        my: 1,
+                        mx: 2,
+                        width: 500,
+                        height: 500,
+                        borderRadius: 5,
+                        bgcolor: '#f0ad4e',
+                    }}>
+                </Box>
             </Box>
+            
         </div>
     );
 };
